8dc890ee6aa53722cabd68592b33639285b3112b
Ilham Berlian O <70630655+IlhamBerlianO@users.noreply.github.com>
2020-09-09 11:30:14 +0700

    Versi 1.2
    Video Ke 2 Restoran-CI4

--- ci4/app/Controllers/Kategori.php
+++ ci4/app/Controllers/Kategori.php
@@ -8,6 +8,36 @@ class Kategori extends BaseController
 		echo "<h1>Belajar CI4</h1>";
 	}
 
+	public function select()
+	{
+		echo "Menampilkan semua data";
+	}
+
+	public function selectWhere($id=null)
+	{
+		echo "Menampilkan semua data yang dipilih";
+	}
+
+	public function formInsert()
+	{
+		echo "Menampilkan form insert";
+	}
+
+	public function formUpdate()
+	{
+		echo "Menampilkan form update";
+	}
+
+	public function update($id=null)
+	{
+		echo "Prses update data $id";
+	}
+
+	public function delete($id=null)
+	{
+		echo "Proses delete data";
+	}
+	
 	//--------------------------------------------------------------------
 
 }
